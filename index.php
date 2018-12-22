d5121550ddf907895781a8eddaba65a09ba86067
JsCore <thanhfgit@gmail.com>
2018-12-22 12:05:24 -0500

    using composer __GOOD

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors', TRUE);
 ini_set('display_startup_errors', TRUE);
 # required bootstrap
+
+require_once 'vendor/autoload.php'; // apply composer :: autoload classmap
 require_once 'core/bootstrap.php';
 
 
